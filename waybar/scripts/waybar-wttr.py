f51f1b0fa3a2c1eafe50cc2c1b81460a2e3045d4
Jason Phan <jase.phan@mail.utoronto.ca>
2025-07-27 22:21:02 -0400

    new weather usage and swaylock update

--- waybar/scripts/waybar-wttr.py
+++ waybar/scripts/waybar-wttr.py
@@ -6,54 +6,46 @@ from datetime import datetime
 from environs import env
 
 WEATHER_CODES = {
-    '113': '☀️ ',
-    '116': '⛅ ',
-    '119': '☁️ ',
-    '122': '☁️ ',
-    '143': '☁️ ',
-    '176': '🌧️',
-    '179': '🌧️',
-    '182': '🌧️',
-    '185': '🌧️',
-    '200': '⛈️ ',
-    '227': '🌨️',
-    '230': '🌨️',
-    '248': '☁️ ',
-    '260': '☁️ ',
-    '263': '🌧️',
-    '266': '🌧️',
-    '281': '🌧️',
-    '284': '🌧️',
-    '293': '🌧️',
-    '296': '🌧️',
-    '299': '🌧️',
-    '302': '🌧️',
-    '305': '🌧️',
-    '308': '🌧️',
-    '311': '🌧️',
-    '314': '🌧️',
-    '317': '🌧️',
-    '320': '🌨️',
-    '323': '🌨️',
-    '326': '🌨️',
-    '329': '❄️ ',
-    '332': '❄️ ',
-    '335': '❄️ ',
-    '338': '❄️ ',
-    '350': '🌧️',
-    '353': '🌧️',
-    '356': '🌧️',
-    '359': '🌧️',
-    '362': '🌧️',
-    '365': '🌧️',
-    '368': '🌧️',
-    '371': '❄️',
-    '374': '🌨️',
-    '377': '🌨️',
-    '386': '🌨️',
-    '389': '🌨️',
-    '392': '🌧️',
-    '395': '❄️ '
+    1: "☀️ ",    # Sunny
+    2: "🌞 ",    # Mostly Sunny
+    3: "⛅ ",    # Partly Sunny
+    4: "🌥️ ",    # Intermittent Clouds
+    5: "🌤️ ",    # Hazy Sunshine
+    6: "☁️ ",    # Mostly Cloudy
+    7: "☁️ ",    # Cloudy
+    8: "🌫️ ",    # Dreary (Overcast)
+    11: "🌫️ ",   # Fog
+    12: "🌧️ ",   # Showers
+    13: "🌧️ ",   # Mostly Cloudy w/ Showers
+    14: "🌦️ ",   # Partly Sunny w/ Showers
+    15: "🌩️ ",   # T-Storms
+    16: "⛈️ ",   # Mostly Cloudy w/ T-Storms
+    17: "🌩️ ",   # Partly Sunny w/ T-Storms
+    18: "🌧️ ",   # Rain
+    19: "🌨️ ",   # Flurries
+    20: "🌨️ ",   # Mostly Cloudy w/ Flurries
+    21: "🌨️ ",   # Partly Sunny w/ Flurries
+    22: "❄️ ",    # Snow
+    23: "❄️ ",    # Mostly Cloudy w/ Snow
+    24: "🧊 ",    # Ice
+    25: "🌨️ ",   # Sleet
+    26: "❄️ ",    # Freezing Rain
+    29: "🌧️❄️ ", # Rain and Snow
+    30: "🔥 ",    # Hot
+    31: "❄️ ",    # Cold
+    32: "🌬️ ",   # Windy
+    33: "🌞 ",    # Clear
+    34: "🌤️ ",   # Mostly Clear
+    35: "⛅ ",    # Partly Cloudy
+    36: "🌥️ ",   # Intermittent Clouds
+    37: "🌕🌫️ ", # Hazy Moonlight
+    38: "☁️ ",    # Mostly Cloudy
+    39: "🌦️ ",   # Partly Cloudy w/ Showers
+    40: "🌧️ ",   # Mostly Cloudy w/ Showers
+    41: "🌩️ ",   # Partly Cloudy w/ T-Storms
+    42: "⛈️ ",   # Mostly Cloudy w/ T-Storms
+    43: "🌨️ ",   # Mostly Cloudy w/ Flurries
+    44: "❄️ ",    # Mostly Cloudy w/ Snow
 }
 
 data = {}
@@ -63,10 +55,11 @@ WEATHER_KEY = env("WEATHER_KEY")
 
 
 location = requests.get("http://ipwho.is/").json()
-city_code = requests.get(f'http://dataservice.accuweather.com/locations/v1/cities/ipaddress?apikey=%20{WEATHER_KEY}&q={location['ip']}').json()
+city_code = requests.get(f'http://dataservice.accuweather.com/locations/v1/cities/search?apikey=%20{WEATHER_KEY}&q={location['city']}').json()
 
 
-weather2 = requests.get(f"http://dataservice.accuweather.com/currentconditions/v1/{city_code['Key']}?apikey=%20{WEATHER_KEY}").json()
+
+weather2 = requests.get(f"http://dataservice.accuweather.com/currentconditions/v1/{city_code[0]['Key']}?apikey=%20{WEATHER_KEY}").json()
 
 weather = requests.get("https://wttr.in/?format=j1").json()
 
@@ -103,11 +96,11 @@ if tempint > 0 and tempint < 10:
     extrachar = '+'
 
 
-data['text'] = ' ' + WEATHER_CODES[weather['current_condition'][0]['weatherCode']] + \
+data['text'] = ' ' + WEATHER_CODES[weather2[0]['WeatherIcon']] + \
     " "+extrachar+str(weather2[0]['Temperature']['Metric']['Value'])+"°"
     
 
-data['tooltip'] = f"<b>{weather['current_condition'][0]['weatherDesc'][0]['value']} {weather['current_condition'][0]['temp_F']}°</b>\n"
+data['tooltip'] = f"<b>{weather2[0]['WeatherText']} {weather2[0]['Temperature']['Metric']['Value']}°</b>\n"
 data['tooltip'] += f"Feels like: {weather['current_condition'][0]['FeelsLikeC']}°\n"
 data['tooltip'] += f"Wind: {weather['current_condition'][0]['windspeedKmph']}Km/h\n"
 data['tooltip'] += f"Humidity: {weather['current_condition'][0]['humidity']}%\n"
@@ -118,13 +111,12 @@ for i, day in enumerate(weather['weather']):
     if i == 1:
         data['tooltip'] += "Tomorrow, "
     data['tooltip'] += f"{day['date']}</b>\n"
-    data['tooltip'] += f"⬆️ {day['maxtempF']}° ⬇️ {day['mintempF']}° "
+    data['tooltip'] += f"⬆️ {day['maxtempC']}° ⬇️ {day['mintempC']}° "
     data['tooltip'] += f"🌅 {day['astronomy'][0]['sunrise']} 🌇 {day['astronomy'][0]['sunset']}\n"
     for hour in day['hourly']:
         if i == 0:
             if int(format_time(hour['time'])) < datetime.now().hour-2:
                 continue
-        data['tooltip'] += f"{format_time(hour['time'])} {WEATHER_CODES[hour['weatherCode']]} {format_temp(hour['FeelsLikeC'])} {hour['weatherDesc'][0]['value']}, {format_chances(hour)}\n"
 
 
 print(json.dumps(data))
